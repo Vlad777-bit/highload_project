acaa56568ba21a56c2f389ab575a73ae69bf1e5e
Vladislav Nozhkin <mega.nozhkin@pm.me>
2022-09-22 14:40:00 +0300

    task 1 is ready

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BubbleSortController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bubble_sort', [BubbleSortController::class, "list"])
+    ->name("bubble_sort");
